19f006ab228e4f2aa7b9a6908c88fd800844497e
AlexandraZaharova <zakharova911@gmail.com>
2021-05-02 21:09:48 +0300

    feat: implement slider in block Pets in Zoo

--- online-zoo/index.js
+++ online-zoo/index.js
@@ -1,3 +1,4 @@
+// pop-up
 const coverElem = document.getElementById('cover');
 const popap = document.getElementById('popap');
 const donateBtn = document.getElementById('donate');
@@ -70,4 +71,96 @@ popapDate.addEventListener('input', () => {
 
 popapCvc.addEventListener('input', () => {
   validate();
+})
+
+// slider Pets in Zoo
+const gap = 30;
+
+const carousel = document.getElementById('carousel');
+const next = document.querySelector('.fourth_screen-gallery-arrow-right');
+const prev = document.querySelector('.fourth_screen-gallery-arrow-left');
+const slides = document.querySelectorAll('.fourth_screen-slider__img');
+const range  = document.querySelector('.fourth-screen__range');
+const textLabel = document.querySelector('.fourth_screen__range-label').firstElementChild;
+
+let slideIndex = 0;
+let imgWidth = document.querySelector('.fourth_screen-slider__img').offsetWidth;
+let width = carousel.offsetWidth;
+let valueNow = range.value;
+
+window.addEventListener('resize', (e) => {
+  width = carousel.offsetWidth;
+  imgWidth = document.querySelector('.fourth_screen-slider__img').offsetWidth;
+});
+let allSlides = document.querySelectorAll('.fourth_screen-slider__img').length;
+let remainingSlides = allSlides - Math.floor(width / imgWidth);
+let visibleSlides = allSlides - remainingSlides;
+
+next.addEventListener('click', e => {
+  slideIndex++;
+  if (slideIndex < slides.length) {
+    // carousel.scrollTo((imgWidth + gap) * slideIndex, 0);
+    if (slideIndex < visibleSlides) {
+      slides[slideIndex-1].firstElementChild.classList.remove('active');
+      slides[slideIndex].firstElementChild.classList.add('active');
+    } else {
+      remainingSlides--;
+      carousel.scrollTo((imgWidth + gap) * (slideIndex - (visibleSlides - 1)), 0);
+      slides[slideIndex-1].firstElementChild.classList.remove('active');
+      slides[slideIndex].firstElementChild.classList.add('active');
+    }
+  } else {
+    slideIndex = 0;
+    carousel.scrollTo(0, 0);
+    slides[slideIndex].firstElementChild.classList.add('active');
+    remainingSlides = allSlides - Math.floor(width / imgWidth);
+  }
+  range.value = slideIndex + 1;
+  valueNow = slideIndex + 1;
+  textLabel.innerText = `0${range.value}/`;
+});
+
+prev.addEventListener('click', e => {
+  if (slideIndex === 0) {
+    slideIndex = slides.length;
+    carousel.scrollTo((imgWidth + gap) * slides.length, 0);
+    slides[slideIndex - 1].firstElementChild.classList.add('active');
+  } else if (slides[slideIndex].offsetLeft > carousel.scrollLeft) {
+    slides[slideIndex].firstElementChild.classList.remove('active');
+    slides[slideIndex-1].firstElementChild.classList.add('active');
+  } else if (slides[slideIndex].offsetLeft === carousel.scrollLeft || slides[slideIndex].offsetLeft < carousel.scrollLeft) {
+    carousel.scrollTo((imgWidth + gap) * (slideIndex - 1), 0);
+    slides[slideIndex].firstElementChild.classList.remove('active');
+    slides[slideIndex-1].firstElementChild.classList.add('active');
+  }
+  slideIndex--;
+  range.value = slideIndex + 1;
+  valueNow = slideIndex + 1;
+  textLabel.innerText = `0${range.value}/`;
+});
+
+range.addEventListener('input', e => {
+  slideIndex = range.value - 1;
+  if (valueNow < range.value) {
+    if (slides[slideIndex].offsetLeft <= carousel.scrollLeft) {
+      slides.forEach(slide => slide.firstElementChild.classList.remove('active'));
+      slides[range.value-1].firstElementChild.classList.add('active');
+    } else if (slides[range.value - 1].offsetLeft > carousel.scrollLeft){
+      remainingSlides--;
+      carousel.scrollTo((imgWidth + gap) * (range.value - visibleSlides), 0);
+      slides.forEach(slide => slide.firstElementChild.classList.remove('active'));
+      slides[range.value-1].firstElementChild.classList.add('active');
+    }
+  } else {
+    if (slides[range.value - 1].offsetLeft >= carousel.scrollLeft) {
+      slides.forEach(slide => slide.firstElementChild.classList.remove('active'));
+      slides[range.value-1].firstElementChild.classList.add('active');
+    } else if (slides[slideIndex].offsetLeft < carousel.scrollLeft) {
+      carousel.scrollTo((imgWidth + gap) * slideIndex, 0);
+      slides.forEach(slide => slide.firstElementChild.classList.remove('active'));
+      slides[range.value-1].firstElementChild.classList.add('active');
+    }
+  }
+  valueNow = range.value;
+  textLabel.innerText = `0${range.value}/`;
 })
